594c77ef79e8096bfbd64cc076e2e4bf96ac489e
steve <stevedanko1@gmail.com>
2014-03-01 19:58:32 -0500

    removed repeated line wher number is changed to a string

--- comma.rb
+++ comma.rb
@@ -1,6 +1,6 @@
 def solution(n)
   number = n.to_s.split("")
-  insert_spot = n.to_s.split("").count - 3
+  insert_spot = number.count - 3
   while insert_spot > 0
     number = number.insert(insert_spot, ",")
     insert_spot -= 3
